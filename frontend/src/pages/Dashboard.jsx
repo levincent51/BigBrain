0e5309ff95b1d0ffc92b2ffcfd24bc367f8b9e72
Vincent Le <levincent51@gmail.com>
2023-04-04 20:03:51 +1000

    update the fetchapi, and welcome and dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
+import fetchAPI from '../utilities/fetch';
 
 const Dashboard = (props) => {
   const { token, setToken } = props;
-
+  const [quizList, setQuizList] = useState([]);
+  useEffect(() => {
+    fetchAPI('GET', props.token, 'admin/quiz')
+      .then((res) => {
+        if (res.error) alert(res.error);
+        console.log(res);
+        setQuizList(res.quizzes);
+      })
+  }, []);
   return (
     <div>
       <Navbar isLoggedin={true} token={token} setToken={setToken}></Navbar>
-      THIS IS THE DASHBOARD
+      <div>
+      <h1>Dashboard</h1>
+      <ul>
+        {quizList.map((quiz) => (
+          <li key={quiz.id}>
+            <h2>{quiz.name}</h2>
+            <p>{quiz.owner}</p>
+            <p>{quiz.id}</p>
+            <p>{quiz.createdAt}</p>
+            <img src={quiz.thumbnail} alt="THERES MEANT TO BE A THUMBNAIL"/>
+            <p>{quiz.active ? 'ACTIVE' : 'NOTACTIVE'}</p>
+            <p>oldSessions:</p>
+            {quiz.oldSessions.map((session) => (
+              <ul key={session}>{session}</ul>
+            ))}
+          </li>
+        ))}
+      </ul>
+      </div>
     </div>
   );
 };
--- frontend/src/utilities/fetch.js
+++ frontend/src/utilities/fetch.js
@@ -1,24 +1,37 @@
 
 const port = 5005;
 
-const fetchAPI = (method, token, path, body = {}) => {
+const fetchAPI = (method, token, path, body = null) => {
   const headers = { 'Content-Type': 'application/json' };
 
   if (token) {
     headers.Authorization = `Bearer ${token}`;
   }
 
-  return fetch(`http://localhost:${port}/${path}`, {
-    method,
-    headers,
-    body: JSON.stringify(body),
-  })
-    .then((response) => {
-      if (response.error) {
-        throw new Error('Login failed');
-      }
-      return response.json();
+  if (body) {
+    return fetch(`http://localhost:${port}/${path}`, {
+      method,
+      headers,
+      body: JSON.stringify(body),
     })
+      .then((response) => {
+        if (response.error) {
+          throw new Error('Login failed');
+        }
+        return response.json();
+      })
+  } else {
+    return fetch(`http://localhost:${port}/${path}`, {
+      method,
+      headers,
+    })
+      .then((response) => {
+        if (response.error) {
+          throw new Error('Login failed');
+        }
+        return response.json();
+      })
+  }
 };
 
 export default fetchAPI;
