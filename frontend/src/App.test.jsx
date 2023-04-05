a41b4fa82d4c77c9418fcae6686489e06d36c3b7
Vincent Le <levincent51@gmail.com>
2023-04-05 17:50:44 +1000

    finished unit test for register

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/o/i); // random letter
+  const linkElement = screen.getByText('Welcome to Big Brain'); // random letter
   expect(linkElement).toBeInTheDocument();
 });
