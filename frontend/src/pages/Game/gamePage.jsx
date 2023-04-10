3e6d9929b420e78906e5b4e1d58d8a366c74dd32
Vincent Le <levincent51@gmail.com>
2023-04-10 11:20:34 +1000

    set up poll template for questions

--- frontend/src/pages/Game/gamePage.jsx
+++ frontend/src/pages/Game/gamePage.jsx
@@ -11,6 +11,7 @@ const GamePage = () => {
   const params = useParams();
   const playerId = params.playerId;
   const [quizStatus, setQuizStatus] = useState('pending');
+  const [questionStarted, setQuestionStarted] = useState('');
 
   useEffect(() => {
     const intervalId = setInterval(async () => {
@@ -20,7 +21,6 @@ const GamePage = () => {
         setQuizStatus('ended');
         console.log('ENDED!!');
       } else if (res.started) {
-        console.log('Startedd!!');
         setQuizStatus('started');
       }
     }, 1000);
@@ -28,6 +28,52 @@ const GamePage = () => {
     return () => clearInterval(intervalId);
   }, [playerId]);
 
+  const fetchQuizResult = async () => {
+    const res = await fetchAPI('GET', null, `play/${playerId}/results`)
+    if (res.error) alert(res.error)
+    else {
+      console.log(res);
+      // TODO THIS IS AN EXAMPLE STRUCTURE OF RES
+      // [
+      //   {
+      //     "answerIds": [
+      //       56513315
+      //     ],
+      //     "correct": false,
+      //     "answeredAt": "2020-10-31T14:45:21.077Z",
+      //     "questionStartedAt": "2020-10-31T14:45:21.077Z"
+      //   }
+      // ]
+    }
+  }
+
+  // THIS IS FOR POLLIN QUESTIOn
+  useEffect(() => {
+    console.log(quizStatus);
+    if (quizStatus === 'started') {
+      // NEED TO KEEP TRACK OF isoTimeLastQuestionStarted if it is isoTimeLastQUestion + timeLimit
+      // call await fetchAPI('GET', null, `play/${playerId}/answer`)
+      // if we find that the poll response res.question.id is different to the next poll, switch back to
+      // the question
+      const intervalId = setInterval(async () => {
+        const res = await fetchAPI('GET', null, `play/${playerId}/question`)
+        if (res.error) {
+          clearInterval(intervalId);
+          setQuizStatus('ended');
+          console.log('ENDED!!');
+        } else {
+          console.log(res.question);
+        }
+      }, 1000);
+      return () => clearInterval(intervalId);
+    }
+    if (quizStatus === 'ended') {
+      fetchQuizResult();
+    }
+  }, [quizStatus]);
+
+  // TODO, when the game has started, switch to questions component page, with timer
+  // TODO WHEN SESSION ENDS, GOES TO RESULT PAGE
   return (
     <Container>
       {quizStatus === 'pending' && <Lobby/>}
