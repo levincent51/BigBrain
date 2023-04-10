7250b5499cebdb98b5bf2f454598835dc331ebe9
mi-mott <98304083+mi-mott@users.noreply.github.com>
2023-04-10 14:16:42 +1000

    button for view game controller added

--- frontend/src/components/Dashboard/GameCard/StopGameButton.jsx
+++ frontend/src/components/Dashboard/GameCard/StopGameButton.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 import { useContext, Context } from '../../../context';
 import Button from '@mui/material/Button';
-import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import StopIcon from '@mui/icons-material/Stop';
 import Popover from '@mui/material/Popover';
 import PopupState, { bindTrigger, bindPopover } from 'material-ui-popup-state';
 import Paper from '@mui/material/Paper';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import { useNavigate } from 'react-router-dom';
 import { endGame } from '../../../utilities/helpers';
+import PreviewIcon from '@mui/icons-material/Preview';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
 
 const StartGameButton = ({ quizId, isActive, setIsActive, fetchAllQuizzes, sessionId }) => {
   const navigate = useNavigate();
@@ -31,45 +34,59 @@ const StartGameButton = ({ quizId, isActive, setIsActive, fetchAllQuizzes, sessi
   }
 
   return (
-    <PopupState variant="popover" popupId="demo-popup-popover">
-      {(popupState) => (
-        <div onClick={stopGame}>
-          <Button
-              variant="contained"
-              endIcon={<PlayCircleOutlineIcon/>}
-              {...bindTrigger(popupState)}
-              sx={{ backgroundColor: 'red', width: '90%', height: '24px', position: 'absolute', bottom: '5%', left: '5%', fontSize: '11px' }}
-            >
-            {'End Quiz'}
-            </Button>
-          <Popover
-          {...bindPopover(popupState)}
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'center',
-          }}
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'center',
-          }}
-          >
-          <Paper
-            component="form"
-            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center' }}
-          >
+    <Box >
+      <PopupState variant="popover" popupId="demo-popup-popover">
+        {(popupState) => (
+          <div onClick={stopGame}>
             <Button
-              variant="outlined"
-              startIcon={<CheckCircleOutlineIcon/>}
-              sx={{ margin: '5px' }}
-              onClick={viewResults}
+              variant="contained"
+              endIcon={<StopIcon/>}
+              {...bindTrigger(popupState)}
+              sx={{ backgroundColor: 'red', height: '24px', fontSize: '11px', width: '75%', position: 'absolute', bottom: '5%', left: '5%' }}
             >
-              Take me to quiz results
+              End Quiz
             </Button>
-          </Paper>
-          </Popover>
-        </div>
-      )}
-    </PopupState>
+            <Popover
+            {...bindPopover(popupState)}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+            >
+            <Paper
+              component="form"
+              sx={{ p: '2px 4px', display: 'flex', alignItems: 'center' }}
+            >
+              <Button
+                variant="outlined"
+                startIcon={<CheckCircleOutlineIcon/>}
+                sx={{ margin: '5px' }}
+                onClick={viewResults}
+              >
+                Take me to quiz results
+              </Button>
+            </Paper>
+            </Popover>
+          </div>
+        )}
+      </PopupState>
+      <IconButton
+        sx={{
+          height: '24px',
+          fontSize: '11px',
+          position: 'absolute',
+          bottom: '5%',
+          right: '0'
+        }}
+        onClick={viewResults}
+      >
+        <PreviewIcon />
+      </IconButton >
+    </Box>
   );
 }
 
